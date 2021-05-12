aa9c4663dfb52cf2b8a5ab2fe0f529b6466cba41
unknown <didiervolk@hotmail.com>
2021-05-12 13:41:47 +0200

    connected edges with begin and end nodes

--- neural_net.py
+++ neural_net.py
@@ -9,37 +9,38 @@ import json
 # Assignment:   Assignment 2 :: symbol recognizer
 
 # Prioritized requirements:
-# - Create a neural net application, that can distuinguish (sloppy handwritten) circles and crosses from each other.
-# - Atleast 2 layers of nodes
-# - The usage of Machine learning libraries is prohibited
+    # - Create a neural net application, that can distuinguish (sloppy handwritten) circles and crosses from each other.
+    # - Atleast 2 layers of nodes
+    # - The usage of Machine learning libraries is prohibited
 
 # Main design choices:
     # - We made two modules, edge.py and node.py. We use modules to break down the program into small manageable and organized files.
     # - Using modules provide reusability of the code.
 
     # Class Edge
-    # - The class Edge is designed that the edges are placed in a list and the beginNode and the endNode are linked to the edge.
+        # - The class Edge is designed that the edges are placed in a list and the beginNode and the endNode are linked to the edge.
 
     # Class Node 
-    # - We designed the class node in a way that the neural net is easy scaleable if you want to add multiple layers.
-    # - The class Node is designed in a way that every node has an incomming edge and an outgoing edge. 
+        # - We designed the class node in a way that the neural net is easy scaleable if you want to add multiple layers.
+        # - The class Node is designed in a way that every node has an incomming edge and an outgoing edge. 
 
 # Test specification:
-# -
-# -
+    # - Check if the nodes are connected correctly with the right edge in between
+    # - Is it possible to insert the data from the trainingset into the node
 
 Node = node.Node
 Edge = edge.Edge
 
-nodeInput = [Node(i, i) for i in range(9)]
-nodeOutput = [Node(i, i) for i in range(2)]
-edges = [Edge(0, 0, 0)]
+nodeInput = [Node(i, i, i, 0) for i in range(9)]
+nodeOutput = [Node(i, i, i, 0) for i in range(2)]
+edges = []
 
 edgeId = 0
+cargo = 0
 
 for nodeIn in nodeInput:
     for nodeOut in nodeOutput:
-        edge = Edge(edgeId, nodeIn, nodeOut)
+        edge = Edge(edgeId, nodeIn, nodeOut, cargo)
         nodeIn.addOutgoingEdge(edge)
         nodeOut.addIncommingEdge(edge)
         edgeId+=1
@@ -49,20 +50,20 @@ t = open('trainingset.json',)
 
 data = json.load(t)
 
-# for i in data['trainingsSet']:
-#     print(data['trainingsSet'][f'{i}']['input'])
+matrix = data['trainingsSet'][f'{5}']['input']
 
-test = [data['trainingsSet'][f'{1}']['input']]
-
-print(test)
-# print("\n======== Input Nodes ========\n")
-# for i in range(len(nodeInput)):
-#     print(i, nodeInput[i])
-# print("\n======== Output Nodes ========\n")
-# for i in range(len(nodeOutput)):
-#     print(i, nodeOutput[i])
-# for i in range(2):
-#     print(i, nodeInput[5].list_outgoingEdges[i])
-# print("\n======== Edges ===============\n")
 # for i in range(len(edges)):
-#     print(i, edges[i])
+#   edges[i].setAmplification()
+
+# edges[4].setAmplification(50000)
+
+for i in range(len(nodeInput)):
+    nodeInput[i].addMatrixInput(matrix[i])
+    print(nodeInput[i].matrixInput)
+
+print('=========================')
+
+print(nodeOutput[0].sigmoid())
+print(nodeOutput[1].sigmoid())
+
+    
--- node.py
+++ node.py
@@ -1,26 +1,14 @@
 import numpy as np
+import edge
+
 # Author:       Ricardo Mokveld || Didier Volk  || Diederik van Linden
 # Studentnr:    0971051         || 0973139      || 0970665
-# Class:        Tinlab Machine Learning
-# Assignment:   Assignment 2 :: symbol recognizer
-
-# Prioritized requirements:
-# - Create a neural net application, that can distuinguish (sloppy handwritten) circles and crosses from each other.
-# - Atleast 2 layers of nodes
-# - The usage of Machine learning libraries is prohibited
-
-# Main design choices:
-# -
-# -
-
-# Test specification:
-# -
-# -
 
 class Node:
-    def __init__(self, outgoingEdge, incommingEdge):
+    def __init__(self, outgoingEdge, incommingEdge, matrixInput, value):
         self.list_outgoingEdges = []   
-        self.list_incommingEdges = []  
+        self.list_incommingEdges = [] 
+        self.value = value 
             
     def addOutgoingEdge(self, outgoingEdge):
         self.list_outgoingEdges.append(outgoingEdge)
@@ -28,5 +16,20 @@ class Node:
     def addIncommingEdge(self, incommingEdge):
         self.list_incommingEdges.append(incommingEdge)
 
-    def sigmoid(x): 
-        return 1/(1 + np.exp(-x))      
+    def addMatrixInput(self, matrixInput):
+        self.matrixInput = matrixInput
+
+
+
+
+    def getValue(self):
+        for i in range(len(self.list_incommingEdges)):
+            self.value += self.list_incommingEdges[i].getValue()
+            
+        return self.value
+            
+    def sigmoid(self): 
+        x = self.getValue()
+        return 1/(1 + np.exp(-x))   
+   
+            
